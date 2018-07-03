a25117d7a7ca5893973fd73471fdfce4687f0a79
JH013 <wangzhuo0902@outlook.com>
2018-07-03 17:37:57 +0800

    StreamXXXIO

--- CodeDemo/Program.cs
+++ CodeDemo/Program.cs
@@ -13,7 +13,12 @@ namespace CodeDemo
         {
             //var ca = FileStreamIO.Read("E:\\12345.txt", 9, 0, "GB2312");
 
-            FileStreamIO.Write("E:\\12345.txt", "mb", 2, "GB2312");
+            //FileStreamIO.Write("E:\\12345.txt", "mb", 2, "GB2312");
+
+            var ji = StreamXXXIO.Read("E:\\12345.txt", "GB2312");
+
+            StreamXXXIO.Write("E:\\12345.txt", "明明", "GB2312");
+
             //var i = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 5);
             //var j = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 4);
             //var k = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 10);
