ee02b703383ff3283c54e0b5575ef85206afaed5
JH013 <wangzhuo0902@outlook.com>
2018-07-03 16:15:26 +0800

    FileStreamIO

--- CodeDemo/Program.cs
+++ CodeDemo/Program.cs
@@ -11,10 +11,12 @@ namespace CodeDemo
     {
         static void Main(string[] args)
         {
-            TxtFileHelper.TxtSpecialLineModify("E:\\12345.txt", 13, "modi13");
-            var i = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 5);
-            var j = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 4);
-            var k = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 10);
+            //var ca = FileStreamIO.Read("E:\\12345.txt", 9, 0, "GB2312");
+
+            FileStreamIO.Write("E:\\12345.txt", "mb", 2, "GB2312");
+            //var i = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 5);
+            //var j = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 4);
+            //var k = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 10);
         }
     }
 }
