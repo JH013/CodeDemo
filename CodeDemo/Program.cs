b5c77e38562e7310c52fc2a16747682d64b6f582
JH013 <wangzhuo0902@outlook.com>
2018-08-27 08:34:41 +0800

    1

--- CodeDemo/Program.cs
+++ CodeDemo/Program.cs
@@ -1,7 +1,9 @@
-﻿using Framework.Log;
-using Framework.PythonInteractive;
-using System;
+﻿using Framework.Http;
 using System.Collections.Generic;
+using System.Threading;
+using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace CodeDemo
 {
@@ -9,227 +11,43 @@ namespace CodeDemo
     {
         static void Main(string[] args)
         {
-            //var ca = FileStreamIO.Read("E:\\12345.txt", 9, 0, "GB2312");
+            //List<Thread> pool = new List<Thread>();
 
-            //FileStreamIO.Write("E:\\12345.txt", "mb", 2, "GB2312");
-
-            //var ji = StreamXXXIO.Read("E:\\12345.txt", "GB2312");
-
-            //StreamXXXIO.Write("E:\\12345.txt", "明明", "GB2312");
-
-            //var i = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 5);
-            //var j = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 4);
-            //var k = TxtFileHelper.TxtSpecialLineRead("E:\\12345.txt", 10);
-
-            //var jj2 = DateTime.Now.ToString("yyyy-MM-dd HH:MM:ss");
-
-            //var start = DateTime.Now;
-            //Thread.Sleep(3000);
-            //var end = DateTime.Now;
-            //TimeSpan duration = end - start;
-            //var jj = duration.Ticks;
-            //if (duration > new TimeSpan(40000000))
+            //for (int i = 0; i <= 100; i++)
             //{
-
+            //    pool.Add(new Thread(TestHttp));
             //}
 
-
-            //Stopwatch stopWatch = new Stopwatch();
-            //stopWatch.Start();
-            //var jj = WebRequestHelper.Get(@"http://192.168.1.150:5000/parse?q=价格怎么样&model=model_20180605-042101");
-            //stopWatch.Stop();
-            //TimeSpan ts = stopWatch.Elapsed;
-            //var time = ts.Milliseconds;
-
-            //System.Timers.Timer timer = new System.Timers.Timer();
-
-            //timer.Interval = 500;
-
-            //timer.Elapsed += delegate
+            //foreach (var thread in pool)
             //{
-            //    Console.WriteLine($"Timer Thread: {Thread.CurrentThread.ManagedThreadId}");
-
-            //    Console.WriteLine($"Is Thread Pool: {Thread.CurrentThread.IsThreadPoolThread}");
-
-            //    Console.WriteLine("Timer Action");
-
-            //    timer.Stop();
-            //};
-
-            //timer.Start();
-
-            //Console.WriteLine("Main Action.");
-            //Console.WriteLine($"Main Thread: {Thread.CurrentThread.ManagedThreadId}");
-
-            //Console.ReadLine();
-
-            //var jj = ComputerInfo.GetWin32ProcessorInfos();
-
-            ILogger logger = Logger.GetInstance(typeof(Program));
-
-            //PerformanceCounterCollect collector = new PerformanceCounterCollect("Processor", "% Processor Time", "_Total");
-
-            //PerformanceCounterCollect collector2 = new PerformanceCounterCollect("Process", "% Processor Time", "ConsoleApp5");
-
-            //PerformanceCounterCollect collector3 = new PerformanceCounterCollect("Process", "Working Set - Private", "ConsoleApp5");
-
-            //ProcessInfo processInfo = new ProcessInfo("ConsoleApp5");
-
-            //while (true)
-            //{
-            //    try
-            //    {
-            //        logger.Info($"CPU Utilization -> {processInfo.CpuUtilization}% --- Memory Usage -> {processInfo.MemoryUsage} MB --- Thread Count -> {processInfo.ThreadCount}");
-            //        Thread.Sleep(1000);
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        logger.Error("Error happened.", ex);
-            //    }
+            //    thread.Start();
             //}
 
+            //var result = WebRequestHelper.Post("http://localhost:8005/api/Test/PostMethod", "{\"Text\":\"price\",\"Type\":\"xdianhu_house\"}", "application/json");
+
+
+            string str = "我不是那么需要呀";
+            Regex reg = new Regex(".*不.*需要.*");
+
+            MatchCollection mc = reg.Matches(str);
+
+            foreach(var m in mc)
+            {
+
+            }
 
 
 
-
-
-
-
-            //SharpZipLibHelper.Decompress(@"E:\ziptest.zip", "E:\\test123");
-
-            //SharpZipLibHelper.Decompress(@"C:\Users\13\Downloads\model_20180704-015925 (1).zip", "E:\\test123");
-
-            //WebRequestHelper.DownloadFile(@"http://192.168.1.249:8083/models/房产/model_20180704-015925.zip", "E:\\123.zip");
-
-            //string jj = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
-
-            //CompressionHelper.Decompress(@"C:\Users\13\Downloads\model_20180704-015925.zip", "E:\\test123");
-
-            //SharpZipLibHelper.Decompress(@"E:\123.zip", "E:\\test123");
-
-            //string filename = @"http://192.168.1.249:8083/models/房产/model_20180704-015925.zip";
-            //var arr = filename.Split('/');
-            //var project = arr[4].ToString();
-            //var model = arr[5].Substring(0, arr[5].Length - 4);
-            //filename = filename.Substring(filename.LastIndexOf(@"/") + 1);
-
-            //var result = dosplit(@"123\n\n45\n678\n\n52\n\n64\n\n", @"\n\n");
-
-            //IronPythonHelper.RunFunc("test.py", "say_hello");
-
-            //ProcessContainer container = new ProcessContainer();
-
-            //container.Add(new Process());
-
-            //var jj = container[1];
-
-            var jj = IronPythonHelper.RunFunc();
-            Console.WriteLine(jj.ToString());
             Console.ReadKey();
         }
 
-
-        public static string[] DoSplit(string text, string sep)
+        public static void TestHttp()
         {
-            var result = new List<string>();
-            RecurrenceSplit(text, sep, result);
-            return result.ToArray();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = WebRequestHelper.Get("http://localhost:8005/api/Test/GetMethod");
+            stopwatch.Stop();
+            Console.WriteLine($"result->{result},pid->{Thread.CurrentThread.ManagedThreadId}, timeout->{stopwatch.ElapsedMilliseconds}");
         }
-
-        public static void RecurrenceSplit(string text, string sep, List<string> result)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                return;
-            }
-            int sepIndex = text.IndexOf(sep);
-            if (sepIndex == -1)
-            {
-                result.Add(text);
-            }
-            else
-            {
-                int tempLength = sepIndex + sep.Length;
-                var temp = text.Substring(0, tempLength);
-                result.Add(temp);
-
-                RecurrenceSplit(text.Substring(tempLength, text.Length - tempLength), sep, result);
-            }
-
-        }
-
-
-        public static string[] dosplit(string text, string sep)
-        {
-            List<string> result = new List<string>();
-            char[] textArr = text.ToCharArray();
-            int index = 0;
-            string tmp = text;
-            while (index < text.Length)
-            {
-                int id = tmp.IndexOf(sep);
-                if (id == -1)
-                {
-                    result.Add(tmp.Substring(0));
-                    break;
-                }
-                else
-                {
-                    id += sep.Length;
-                    result.Add(tmp.Substring(0, id));
-                    index += id;
-                    tmp = tmp.Substring(id);
-                }
-            }
-            return result.ToArray();
-        }
-
-
-        //public static string[] StringSplit(string text, string separator)
-        //{
-        //    List<string> result = new List<string>();
-        //    char[] textArr = text.ToCharArray();
-        //    int index = 0;
-        //    for (int i = 0; i <= textArr.Length - 1;)
-        //    {
-        //        if (char.Equals(textArr[i], separator.First()))
-        //        {
-        //            var seLength = GetLength(textArr, i, separator);
-        //            var length = i - index + seLength;
-        //            result.Add(text.Substring(index, length));
-        //            i = index += length;
-        //        }
-        //        else
-        //        {
-        //            i++;
-        //        }
-        //    }
-        //    if (index < textArr.Length - 1)
-        //    {
-        //        result.Add(text.Substring(index, text.Length - index));
-        //    }
-        //    return result.ToArray();
-        //}
-
-        //public static int GetLength(char[] textArr, int index, string separator)
-        //{
-        //    int length = separator.Length;
-        //    while (index + length <= textArr.Length)
-        //    {
-        //        if (index + length > textArr.Length - 1)
-        //        {
-        //            break;
-        //        }
-        //        if (char.Equals(textArr[index + length], separator.First()))
-        //        {
-        //            length += separator.Length;
-        //        }
-        //        else
-        //        {
-        //            break;
-        //        }
-        //    }
-        //    return length;
-        //}
     }
 }
